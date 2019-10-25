e6c1f0eecc9f27c6dcb12d76a393db7577f288fb
Sansiel <deviler.san@yandex.ru>
2019-10-25 21:05:39 +0400

    try_to_work

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000/api'
+  apiUrl: 'https://52.200.110.231:8000/api'
+  //apiUrl: 'http://localhost:8000/api'
 };
 
 /*
